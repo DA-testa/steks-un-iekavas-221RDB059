881ed49d863106e6b27fb366fc66810edd587a76
221RDB059 <125189798+221RDB059@users.noreply.github.com>
2023-02-12 13:16:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-# python3
-
-from collections import namedtuple
-
-Bracket = namedtuple("Bracket", ["char", "position"])
-
-
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
-
-
-def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+def bracket_checker(text):
+    stack = []
+    for i in range(len(text)):
+        if text[i] in OPENING_BRACKETS:
+            stack.append(text[i])
+        elif text[i] in CLOSING_BRACKETS:
+            bracket_type = CLOSING_BRACKETS[OPENING_BRACKETS.index(
+                stack[-1])]  # Oh god
+            if bracket_type == text[i]:
+                stack.pop(-1)
+            else:
+                return i+1
+    if len(stack) != 0:
+        return len(text)
+    return "Success"
 
 
-def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+OPENING_BRACKETS = ('(', '[', '{')
+CLOSING_BRACKETS = (')', ']', '}')
 
+text = input()
 
-if __name__ == "__main__":
-    main()
+print(bracket_checker(text))
