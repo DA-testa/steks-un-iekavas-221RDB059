0412e8b0da75fd05db250f192d1e52ecd1144dc2
221RDB059 <125189798+221RDB059@users.noreply.github.com>
2023-02-12 14:38:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import os
+
+
 def bracket_checker(text):
-    # if text[0:5] == "I\\r\\n":
-    #     text = text[5:]
     stack = []
     for i in range(len(text)):
         if text[i] in OPENING_BRACKETS:
             stack.append(text[i])
         elif text[i] in CLOSING_BRACKETS:
+            if len(stack) == 0:
+                return i+1
             bracket_type = CLOSING_BRACKETS[OPENING_BRACKETS.index(
                 stack[-1])]  # Oh god
             if bracket_type == text[i]:
@@ -19,7 +22,29 @@ def bracket_checker(text):
 
 OPENING_BRACKETS = ('(', '[', '{')
 CLOSING_BRACKETS = (')', ']', '}')
-input()
-text = input()
+file_or_input = input()
+if file_or_input.lower() == "i":
+    text = input()
+    print(bracket_checker(text))
+elif file_or_input.lower() == "f":
+    test_amount = len(os.listdir("test/"))
+    good_tests = 0
+    total_tests = 0
+    for i in range(test_amount // 2):
+        print(f"test {i+1}____________________________________________")
 
-print(bracket_checker(text))
+        with open(f"test/{i}") as f:
+            lines = f.readline()
+            answer = bracket_checker(lines)
+            print(f"your answer: {answer}")
+        with open(f"test/{i}.a") as f:
+            lines = f.readline()
+            print(f"actual answer: {lines}")
+            if str(answer) == lines:
+                print("good")
+                good_tests += 1
+        total_tests += 1
+        i += 1
+        print()
+else:
+    print("Unknown command")
